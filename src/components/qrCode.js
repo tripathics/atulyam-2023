1c45dd24cab6cb4e9b867b3e51efc2b384da05e6
Pursottam Sah <rahulsah6003@gmail.com>
2023-04-27 01:19:43 +0530

    added qr code scanner with its styles in user.js

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -8,12 +8,12 @@ const details="Hello gusys i am pursottam sah a student of computer science";
 const QrCode = ({value}) => {
   return (
     <>
-    <div style={{ height: "auto", margin: "0 auto", maxWidth: 100, width: "100%" }}>
+    <div style={{ height: "auto", margin: "0 auto", maxWidth: 150, width: "100%" }}>
     <QRCode
     size={300}
     style={{ height: "auto", maxWidth: "100%", width: "100%" }}
     value={value}
-    viewBox={`0 0 300 300`}
+    viewBox={`0 0 400 400`}
     />
     </div>
     </>
